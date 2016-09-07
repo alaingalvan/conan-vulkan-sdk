2a90e94abe5ede5c879eb2f86bf086516f9cf165
Alain Galvan <hi@alain.xyz>
2016-09-07 17:35:28 -0400

    Update conanfile

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # conan-vulkan-sdk
 
-![Release][release-img] [![conan-img]][conan-url] [![License][license-img]][license-url]
+![Release][release-img]
+[![conan-img]][conan-url]
+[![License][license-img]][license-url]
 
 > **NOTE**: I have no affiliation with the Khronos Group. 
 
@@ -9,33 +11,11 @@
 
 ## Usage
 
-Install [Python](https://www.python.org/downloads/), [Visual Studio](https://www.visualstudio.com/en-us/visual-studio-homepage-vs.aspx), and [Conan](https://www.conan.io/), a C++ Package Manager, 
-
-On Windows make sure that your path includes Visual Studio's `msbuild` as well as `python`.
+Install [Python](https://www.python.org/downloads/), and [Conan](https://www.conan.io/), a C++ Package Manager, and install the [Vulkan SDK](https://vulkan.lunarg.com/signin) separately.
 
 ```bash
 conan install conan-vulkan-sdk --build
 ```
-
-## Features
-
-- **Windows/Linux Support** - This package supports those platforms, with android/osx on its way!
-
-- **Vulkan's Header Files** - Gives you access to all of Vulkan's core functionality.
-
-- **ICD Loader** - Handles Multi-GPU support as well as loading the different layers of Vulkan.
-
-- **Validation Layers** - Enables error checking on the Vulkan Driver.
-
-## Development
-
-Build specs based on instructions on the [Vulkan Repo](https://github.com/KhronosGroup/Vulkan-LoaderAndValidationLayers/blob/master/BUILD.md), as well as what I was able to see from the sdk binaries. 
-
-Versions correspond with releases on the KhronosGroup Repo. Please ask the Khronos Group to distribute binaries! 
-
-### Call to Action
-
-Let's write a Heroku app that automatically releases the latest version of the sdk to conan when the Khronos Group Repo is updated! 
 
 [release-img]: https://img.shields.io/badge/release-1.0.21.0-B46BD6.svg?style=flat-square
 [conan-img]: https://img.shields.io/badge/conan.io-1.0.21.0-green.svg?style=flat-square
--- conanfile.py
+++ conanfile.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from conans import ConanFile, tools
-import sys
-import os
+from conans import ConanFile
 
 class vulkansdkConan(ConanFile):
 
@@ -16,60 +14,19 @@ class vulkansdkConan(ConanFile):
     foldername = 'VulkanTools-sdk-1.0.21.0'
     builddir = ''
 
-    def source(self):
-        zip_name = 'VulkanTools-sdk-1.0.21.0.zip'
-        tools.download('https://github.com/LunarG/VulkanTools/archive/sdk-1.0.21.0.zip', zip_name)
-        tools.unzip(zip_name)
-        os.unlink(zip_name)
-        os.rename(self.foldername, 'v')
-        self.foldername = 'v' # To Get Around Path being too long errors.
-
-    def system_requirements(self):
-        if self.settings.os == 'Linux':
-            self.run('sudo apt-get install git cmake build-essential bison libx11-dev libxcb1-dev')
-        elif self.settings.os == 'Macos':
-            self.output.error('  ***************** Need Help here **************** ')
-            self.output.error('  *** OSX not implemented yet. Please help out! *** ')
-            self.output.error('  ***************** Need Help here **************** ')
-            sys.exit(1)
-        elif self.settings.os == 'Android':
-            self.output.error('  ***************** Need Help here **************** ')
-            self.output.error('  * Android not implemented yet. Please help out! * ')
-            self.output.error('  ***************** Need Help here **************** ')
-            sys.exit(1)
-
     def build(self):
-        self.builddir = os.path.join(self.conanfile_directory, self.foldername)
-
-        if self.settings.os == 'Windows':
-            if self.settings.arch == 'x86_64':
-                arch = 'x64'
-            else:
-                arch = 'x86'
-            self.run('cd %s & update_external_sources.bat --all' % self.builddir)
-            self.run('cd %s & build_windows_targets.bat' % self.builddir)
-            self.run('cd %s & msbuild build/ALL_BUILD.vcxproj /p:Platform=%s /p:Configuration=%s /m /verbosity:m' % (self.builddir, arch, self.settings.build_type))
-        elif self.settings.os == 'Linux':
-            self.run('./update_external_sources.sh')
-            self.run('cmake -H. -Bdbuild -DCMAKE_BUILD_TYPE=%s' % self.settings.build_type)
-            self.run('cd dbuild')
-            self.run('make')
-            self.run('export LD_LIBRARY_PATH=%s/Vulkan-LoaderAndValidationLayers/dbuild/loader' % self.conanfile_directory)
-            self.run('export VK_LAYER_PATH=%s/Vulkan-LoaderAndValidationLayers/dbuild/layers' % self.conanfile_directory)
-            self.run('cd ../')
+        self.builddir = 'C:/VulkanSDK/1.0.21.1'
 
     def package(self):
-        self.copy(pattern='*', dst='include/vulkan', src='%s/include' % self.builddir, keep_path=False)
-        self.copy('*', dst='include/vulkan', src='%s/external/spirv-tools/external/spirv-headers/include/spirv/1.1' % self.builddir)
-        self.copy('icd-spv.h', dst='include/vulkan', src='%s/tests' % self.builddir)
+        self.copy(pattern='*', dst='Include/vulkan', src='%s/include' % self.builddir, keep_path=False)
 
-        self.copy('*', dst='lib', src='%s/build/loader/%s' % (self.builddir, self.settings.build_type))
-        self.copy('*', dst='lib', src='%s/build/layers/%s' % (self.builddir, self.settings.build_type))
+        is_32 = '' if self.settings.arch == 'x86_64' else '32'
+        bin_dir = 'Bin%s' % is_32
+        if self.settings.build_type == 'Debug':
+            bin_dir = 'Source/lib%s' % is_32
 
-        self.copy('*', dst='bin', src='%s/build/loader/%s' % (self.builddir, self.settings.build_type))
-        self.copy('*', dst='bin', src='%s/build/layers/%s' % (self.builddir, self.settings.build_type))
-        self.copy('*', dst='bin', src='%s/external/glslang/build/StandAlone/%s' % (self.builddir, self.settings.build_type))
-        self.copy('*', dst='bin', src='%s/external/spirv-tools/build/tools/%s' % (self.builddir, self.settings.build_type))
+        self.copy('*', dst='lib', src='%s/%s' % (self.builddir, bin_dir))
+        self.copy('*', dst='bin', src='%s/%s' % (self.builddir, bin_dir))
 
     def package_info(self):
         self.cpp_info.libs = [
@@ -79,12 +36,10 @@ class vulkansdkConan(ConanFile):
             'VkLayer_unique_objects',
             'VkLayer_threading',
             'VkLayer_swapchain',
-            'VkLayer_swapchain',
+            'VkLayer_screenshot',
             'VkLayer_parameter_validation',
             'VkLayer_object_tracker',
             'VkLayer_image',
-            'vkLayer_device_limits',
             'VkLayer_core_validation',
-            'vkLayer_api_dump'
+            'VkLayer_api_dump'
             ]
-
